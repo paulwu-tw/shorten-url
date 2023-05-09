f57eacac297db326fdca1649aa7770756db25417
paul.wu <tease0623@gmail.com>
2023-05-09 23:07:18 +0800

    project init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const routes = require('./routes')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('hbs', hbs.engine({
+    defaultLayout: 'main',
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(routes)
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Listening on http://localhost:${port}`)
+})
